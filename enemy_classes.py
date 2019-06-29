b83836d2274e16e500b60a070b6ca0a29a0c4a75
Will Taylor <w1ll100@hotmail.co.uk>
2019-06-29 18:09:18 +0100

    Pull out repeated update logic

--- enemy_classes.py
+++ enemy_classes.py
@@ -1,10 +1,10 @@
-from game_objects import Sprite, gravity
+from game_objects import Sprite, VelocityUpdates, BlockedByTerrain
 import pygame
 import random
 from collections import deque
 
 
-class AssaultSoldier:
+class AssaultSoldier(BlockedByTerrain):
     def __init__(self,
                  initial_pos=None,
                  move_speed=15,
@@ -25,48 +25,10 @@ class AssaultSoldier:
         self.update_pos(dt, terrain)
 
     def update_velocity(self, dt):
-        self.y_vel = self.y_vel + dt * gravity
-        self.y_vel -= self.drag * self.y_vel * dt
+        VelocityUpdates.gravity(self, dt)
+        VelocityUpdates.drag(self, dt)
         if not self.in_air and random.random() > 0.999:
             self.x_vel *= -1
-
-    def update_pos(self, dt, terrain):
-        hitbox = self.sprite.hitbox
-        new_x = self.sprite.x + dt * self.x_vel
-        new_y = self.sprite.y + dt * self.y_vel
-        moved_x_hb = hitbox.copy()
-        moved_x_hb.x = new_x
-
-        moved_y_hb = hitbox.copy()
-        moved_y_hb.y = new_y
-
-        x_ok, y_ok = True, True
-
-        for hb in [terrain_object.hitbox for terrain_object in terrain]:
-            if hb.top < hitbox.bottom and hb.bottom > hitbox.top:
-                if self.x_vel > 0 and moved_x_hb.left < hb.left <= moved_x_hb.right:
-                    hitbox.right = hb.left
-                    self.x_vel = 0
-                    x_ok = False
-                elif self.x_vel < 0 and moved_x_hb.right > hb.right >= moved_x_hb.left:
-                    hitbox.left = hb.right
-                    self.x_vel = 0
-                    x_ok = False
-
-            if hb.left < hitbox.right and hb.right > hitbox.left:
-                if self.y_vel > 0 and moved_y_hb.top < hb.top <= moved_y_hb.bottom:
-                    hitbox.bottom = hb.top
-                    self.y_vel = 0
-                    self.in_air = False
-                    y_ok = False
-                elif self.y_vel < 0 and moved_y_hb.bottom > hb.bottom > moved_y_hb.top:
-                    hitbox.top = hb.bottom
-                    self.y_vel = 0
-                    y_ok = False
-        if x_ok:
-            self.sprite.x = new_x
-        if y_ok:
-            self.sprite.y = new_y
 
     def draw(self, surface, coordinate_map):
         image, pos = self.sprite.get_sprite()
--- game_objects.py
+++ game_objects.py
@@ -3,6 +3,16 @@ import pygame
 gravity = 2.3
 
 
+class VelocityUpdates:
+    @staticmethod
+    def gravity(self, dt):
+        self.y_vel = self.y_vel + dt * gravity
+
+    @staticmethod
+    def drag(self, dt):
+        self.y_vel -= self.drag * self.y_vel * dt
+
+
 class Sprite:
     def __init__(self,
                  animations,
@@ -44,69 +54,7 @@ class Sprite:
         self.hitbox.top = self.image_pos.y = round(y)
 
 
-class Controls:
-    up = pygame.K_w
-    left = pygame.K_a
-    right = pygame.K_d
-
-
-class Player:
-    def __init__(self,
-                 initial_pos=(0, 0),
-                 initial_vel=(0, 0),
-                 animations=None,
-                 initial_animation="neutral",
-                 fire_gun=None):
-        self.sprite = Sprite(animations=animations,
-                             initial_pos=initial_pos,
-                             initial_animation=initial_animation)
-        self.x_vel, self.y_vel = initial_vel
-        self.jetpack_power = 5.5
-        self.shoot_delay = 2
-        self.till_next_shot = 0
-        self.fire_gun = fire_gun
-        self.drag = 0.03
-        self.in_air = False
-        self.move_speed = 7
-
-    def update(self, pressed, dt, terrain):
-        self.update_velocity(pressed, dt)
-
-        current_player_animation = "neutral"
-        if self.in_air:
-            if pressed[pygame.K_a]:
-                current_player_animation = "fly_left"
-            elif pressed[pygame.K_d]:
-                current_player_animation = "fly_right"
-            elif pressed[pygame.K_w]:
-                current_player_animation = "fly_neutral"
-        self.sprite.update(dt, current_player_animation)
-        self.update_pos(dt, terrain)
-        self.till_next_shot -= dt
-
-    def update_velocity(self, inputs, dt):
-        if inputs[Controls.up]:
-            self.y_vel -= dt * self.jetpack_power
-            self.in_air = True
-        self.y_vel = self.y_vel + dt * gravity
-        self.y_vel -= self.drag * self.y_vel * dt
-
-        if self.in_air:
-            x_accel_sum = 0
-            if inputs[Controls.left]:
-                x_accel_sum -= dt * self.jetpack_power / 3
-            if inputs[Controls.right]:
-                x_accel_sum += dt * self.jetpack_power / 3
-            self.x_vel += x_accel_sum
-            self.x_vel -= self.drag * self.x_vel * dt
-        else:
-            x_vel_sum = 0
-            if inputs[Controls.left]:
-                x_vel_sum -= self.move_speed
-            if inputs[Controls.right]:
-                x_vel_sum += self.move_speed
-            self.x_vel = x_vel_sum
-
+class BlockedByTerrain:
     def update_pos(self, dt, terrain):
         hitbox = self.sprite.hitbox
         new_x = self.sprite.x + dt * self.x_vel
@@ -144,6 +92,70 @@ class Player:
             self.sprite.x = new_x
         if y_ok:
             self.sprite.y = new_y
+
+
+class Controls:
+    up = pygame.K_w
+    left = pygame.K_a
+    right = pygame.K_d
+
+
+class Player(BlockedByTerrain):
+    def __init__(self,
+                 initial_pos=(0, 0),
+                 initial_vel=(0, 0),
+                 animations=None,
+                 initial_animation="neutral",
+                 fire_gun=None):
+        self.sprite = Sprite(animations=animations,
+                             initial_pos=initial_pos,
+                             initial_animation=initial_animation)
+        self.x_vel, self.y_vel = initial_vel
+        self.jetpack_power = 5.5
+        self.shoot_delay = 2
+        self.till_next_shot = 0
+        self.fire_gun = fire_gun
+        self.drag = 0.03
+        self.in_air = False
+        self.move_speed = 7
+
+    def update(self, pressed, dt, terrain):
+        self.update_velocity(pressed, dt)
+
+        current_player_animation = "neutral"
+        if self.in_air:
+            if pressed[pygame.K_a]:
+                current_player_animation = "fly_left"
+            elif pressed[pygame.K_d]:
+                current_player_animation = "fly_right"
+            elif pressed[pygame.K_w]:
+                current_player_animation = "fly_neutral"
+        self.sprite.update(dt, current_player_animation)
+        self.update_pos(dt, terrain)
+        self.till_next_shot -= dt
+
+    def update_velocity(self, inputs, dt):
+        if inputs[Controls.up]:
+            self.y_vel -= dt * self.jetpack_power
+            self.in_air = True
+        VelocityUpdates.gravity(self, dt)
+        VelocityUpdates.drag(self, dt)
+
+        if self.in_air:
+            x_accel_sum = 0
+            if inputs[Controls.left]:
+                x_accel_sum -= dt * self.jetpack_power / 3
+            if inputs[Controls.right]:
+                x_accel_sum += dt * self.jetpack_power / 3
+            self.x_vel += x_accel_sum
+            self.x_vel -= self.drag * self.x_vel * dt
+        else:
+            x_vel_sum = 0
+            if inputs[Controls.left]:
+                x_vel_sum -= self.move_speed
+            if inputs[Controls.right]:
+                x_vel_sum += self.move_speed
+            self.x_vel = x_vel_sum
 
     def shoot_at(self, target_pos):
         if self.till_next_shot <= 0:
